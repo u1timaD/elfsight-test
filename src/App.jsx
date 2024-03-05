eebcee709a61a419485816cfc5967a9685059234
Dmitry <free-doom46@mail.ru>
2024-03-05 10:09:08 +0300

    refactor: del style.scc, change GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
-
-
-const GlobalStyle = createGlobalStyle`
-
-#root {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  text-align: center;
-}
-
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  min-height: 100vh;
-  display: grid;
-  place-content: center;
-  text-align: center;
-  font-size: 14px;
-}
-
-ul,
-li {
-  list-style: none;
-}
-`;
-
 const App = () => {
   return (
     <div>
-      <GlobalStyle />
-      <Header /> 
+      <Header />
       <Main />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,57 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+#root {
+  
+	font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+	max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  text-align: center;
+  font-size: 14px;
+}
+
+ul,
+li {
+  list-style: none;
+}
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
